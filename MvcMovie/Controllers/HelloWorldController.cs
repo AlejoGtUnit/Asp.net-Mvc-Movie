20be922cc71ce798c9c08f583541027ae98c1f9f
Alejandro Hurtado Mijango <3plgtalejandrohm@caexlog.local>
2016-08-04 16:55:54 -0600

    Se agrego el modelo Movie y el scaffold del mismo

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -9,15 +9,17 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET: /HelloWorld
-        public string Index()
+        public ViewResult Index()
         {
-            return "Esta es mi accion por <b>defecto</b>...";
+            return View();
         }
 
         //GET: /HelloWorld/Welcome
-        public string Welcome(string name, int ID = 1)
+        public ActionResult Welcome(string name, int numTimes = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+            ViewBag.Message = "Hola " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
         }
     }
 }
